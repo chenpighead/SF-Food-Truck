e25c76e3c0751cc55459cf1af48dd8d9fafc9047
Jeremy Chen <chenpighead@gmail.com>
2019-01-28 23:21:26 +0800

    Init - build initial data source
    Download foodtruck-rows.json from DataSF:
    https://data.sfgov.org/Economy-and-Community/Mobile-Food-Facility-Permit/rqzj-sfat
    
    Write a simple parser.js to parse data in mongo

---
+++ parser.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const raw = require('foodtruck-rows.json');
+const axios = require('axios');
+
+const columns = raw.meta.view.columns;
+const names = columns.map((column) => {
+  return column.name;
+});
+const data = raw.data;
+
+// console.log(names);
+for (let item of data) {
+  let object = {};
+  for (let i = 0; i < names.length; i++) {
+    if (names[i] === 'id') {
+      object['uuid'] = item[i];
+    } else {
+      object[names[i]] = item[i];
+    }
+  }
+
+  axios
+    .post('http://localhost:3000/api/shops', object)
+    .then(function(response) {
+      console.log(response);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+}
