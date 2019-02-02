52f778a5b52299dc6ff08e202c7aa8c42bbe0c26
Jeremy Chen <chenpighead@gmail.com>
2019-02-02 19:08:42 +0800

    Add feature - support for searching nearby food trucks
    Expose an API for querying shop/food name with nearby distance.
    
    In this patch, we introduce loopback's GeoPoint type while parsing open data
    into our DB, so we can implement near filter easily.
    
    Since we haven't support getLocation() from front-end yet, we use Uber HQ
    as reference location for now.
    
    This API is just for showing the capability of adding such API, and we shall
    extend this once we have front-end support.

--- common/models/shop.js
+++ common/models/shop.js
@@ -1,4 +1,30 @@
 
 module.exports = function (Shop) {
-
+  Shop.nearby = async function (name, lat, lon) {
+    // Reference location for getting nearby shops. We use Uber HQ for now,
+    // but can be modified to user's current location.
+    var userLocation = { lat: 37.7811489, lng: -122.4579986 }
+    let result = await Shop.find({
+      '$text': {
+        search: name
+      },
+      where: {
+        geoLocation: {
+          near: userLocation,
+          maxDistance: 2,
+          unit: 'kilometers'
+        },
+        Status: 'APPROVED'
+      }
+    })
+    return result
+  }
+  Shop.remoteMethod('nearby', {
+    accepts: [
+      { arg: 'name', type: 'string' },
+      { arg: 'lat', type: 'number' },
+      { arg: 'lon', type: 'number' }],
+    returns: { type: 'array', root: true },
+    http: { verb: 'get' }
+  })
 }
--- parser.js
+++ parser.js
@@ -8,12 +8,17 @@ const names = columns.map((column) => {
 })
 const data = raw.data
 
-// console.log(names);
+console.log(names)
 for (let item of data) {
   let object = {}
   for (let i = 0; i < names.length; i++) {
     if (names[i] === 'id') {
+      // rename 'id' to 'uuid'
       object['uuid'] = item[i]
+    } else if (names[i] === 'Location') {
+      // add extra entry to just store geo location
+      object['geoLocation'] = [Number(item[i][1]), Number(item[i][2])]
+      object[names[i]] = item[i]
     } else {
       object[names[i]] = item[i]
     }
