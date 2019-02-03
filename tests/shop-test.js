ec9e9f3afcf6af325c1a32323b0b58442dbb2d9b
Jeremy Chen <chenpighead@gmail.com>
2019-02-03 22:46:26 +0800

    Fix test - separate API tests
    Since we've hidden write-related APIs from the public, move these tests
    out of the current scope.

--- tests/shop-test.js
+++ tests/shop-test.js
@@ -3,14 +3,11 @@ const app = require('../server/server')
 require('should')
 const request = require('supertest')(app)
 
-describe('API Shop tests', function () {
-  before(async function () {
-    const Shop = app.models.Shop
-    await Shop.create({ name: 'shop1' })
-    await Shop.create({ name: 'shop2' })
-    await Shop.create({ name: 'shop3' })
-  })
-
+/*
+ * Test the /POST, /PUT, /DELETE route
+ * XXX: These tests are skipped for now since we hide these APIs at the moment.
+ */
+describe.skip('API Shop POST/PUT/DELETE tests', function () {
   it('should create a shop', function (done) {
     const name = 'shop1'
     request
@@ -20,12 +17,21 @@ describe('API Shop tests', function () {
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
-        console.log(res.body)
-
         res.body.name.should.equal(name)
-
         done()
       })
+  })
+})
+
+/*
+ * Test the /GET route
+ */
+describe('API Shop GET tests', function () {
+  before(async function () {
+    const Shop = app.models.Shop
+    await Shop.create({ name: 'shop1' })
+    await Shop.create({ name: 'shop2' })
+    await Shop.create({ name: 'shop3' })
   })
 
   it('should get shops', function (done) {
@@ -35,7 +41,9 @@ describe('API Shop tests', function () {
       .expect(200)
       .end(function (err, res) {
         if (err) return done(err)
-        console.log(res.body)
+        res.body[0].name.should.equal('shop1')
+        res.body[1].name.should.equal('shop2')
+        res.body[2].name.should.equal('shop3')
         done()
       })
   })
